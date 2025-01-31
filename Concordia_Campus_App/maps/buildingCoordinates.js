fc62bde31fb4e3cc0f7ed171d453b99ab0585ff7
jonathan.dannessa <jonathan.dannessa@ivans.com>
2025-01-31 18:18:51 -0500

    adding more colored buildings

--- Concordia_Campus_App/maps/buildingCoordinates.js
+++ Concordia_Campus_App/maps/buildingCoordinates.js
@@ -14,7 +14,7 @@ const buildingsData = {
         { "latitude": 45.495359016476655, "longitude": -73.57936618647074 }
       ],
       "fillColor": "#800000",
-      "strokeColor": "#800000",
+      "strokeColor": "#000000",
       "popUp": { "name": "John Molson School of Business" }
     },
     {
@@ -29,7 +29,7 @@ const buildingsData = {
           { "latitude": 45.493827023515045, "longitude": -73.57906268634575 }
         ],
       "fillColor": "#800000",
-      "strokeColor": "#800000",
+      "strokeColor": "#000000",
       "popUp": { "name": "Faubourg Tower" }
     },
     {
@@ -44,7 +44,7 @@ const buildingsData = {
         { "latitude": 45.49604560545516, "longitude": -73.57771621742774 }
       ],
       "fillColor": "#800000", 
-      "strokeColor": "#800000",
+      "strokeColor": "#000000",
       "popUp": { "name": "FB Building" }
     },
     {
@@ -56,9 +56,333 @@ const buildingsData = {
         { "latitude": 45.49737389258885, "longitude": -73.57834653654672 }
       ],
       "fillColor": "#800000", 
-      "strokeColor": "#800000",
+      "strokeColor": "#000000",
       "popUp": { "name": "EV Building" }
+    },
+    {
+      "name": "Centre for Structural and Functional Genomics (GE)",
+      "coordinates": [
+        { "latitude": 45.45679964289688, "longitude": -73.64035423599351 },
+        { "latitude": 45.45694639401358, "longitude": -73.64073510968682 },
+        { "latitude": 45.45717592704571, "longitude": -73.64056881272212 },
+        { "latitude": 45.457036702203276, "longitude": -73.64016648135596 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Centre for Structural and Functional Genomics (GE)" }
+    },
+    {
+      "name": "Communication Studies and Journalism Building",
+      "coordinates": [
+        { "latitude": 45.457183452703084, "longitude": -73.64039715133923 },
+        { "latitude": 45.457333965639464, "longitude": -73.64071365201396 },
+        { "latitude": 45.45758983670931, "longitude": -73.64050443970355 },
+        { "latitude": 45.457657567092255, "longitude": -73.6406224569043 },
+        { "latitude": 45.45782312990812, "longitude": -73.6404883464489 },
+        { "latitude": 45.457619939111794, "longitude": -73.64004846415523 },
+        { "latitude": 45.4574844781741, "longitude": -73.64015575251953 },
+        { "latitude": 45.457480715365634, "longitude": -73.63982315859016 },
+        { "latitude": 45.4573189143639, "longitude": -73.63978024324445 },
+        { "latitude": 45.457217318148786, "longitude": -73.64001627764593 },
+        { "latitude": 45.457360305362016, "longitude": -73.6400752862463 },
+        { "latitude": 45.45740169632978, "longitude": -73.64022012553812 },
+        { "latitude": 45.45717968987451, "longitude": -73.64039715133923 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Communication Studies and Journalism Building" }
+    },
+    {
+      "name": "Concordia Gymnasium",
+      "coordinates": [
+        { "latitude": 45.45715335006858, "longitude": -73.63834257922412 },
+        { "latitude": 45.45704046504238, "longitude": -73.63803680738583 },
+        { "latitude": 45.45700659949048, "longitude": -73.6380636294769 },
+        { "latitude": 45.456961445389574, "longitude": -73.63792951902151 },
+        { "latitude": 45.45702165084939, "longitude": -73.63787587483935 },
+        { "latitude": 45.456720622907596, "longitude": -73.63710339861632 },
+        { "latitude": 45.45639701607785, "longitude": -73.63737698394532 },
+        { "latitude": 45.45669052002503, "longitude": -73.63813873133192 },
+        { "latitude": 45.45679211718927, "longitude": -73.63806899389512 },
+        { "latitude": 45.45684479712831, "longitude": -73.63818701109587 },
+        { "latitude": 45.45677706576909, "longitude": -73.63824601969624 },
+        { "latitude": 45.45688618847445, "longitude": -73.63855179153452 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Concordia Gymnasium" }
+    },
+    {
+      "name": "PERFORM Centre",
+      "coordinates": [
+        { "latitude": 45.45701412517042, "longitude": -73.63783832391185 },
+        { "latitude": 45.457281286158434, "longitude": -73.63763447601966 },
+        { "latitude": 45.4569463940146, "longitude": -73.63678153352338 },
+        { "latitude": 45.45668299430188, "longitude": -73.6369907458338 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "PERFORM Centre" }
+    },
+    {
+      "name": "Stinger Dome",
+      "coordinates": [
+        { "latitude": 45.457375356636256, "longitude": -73.63707657655515 },
+        { "latitude": 45.458334866457136, "longitude": -73.63596614198453 },
+        { "latitude": 45.457932250598894, "longitude": -73.63524730994364 },
+        { "latitude": 45.456961445398306, "longitude": -73.63635774451426 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Stinger Dome" }
+    },
+    {
+      "name": "Concordia Stadium",
+      "coordinates": [
+        { "latitude": 45.45782312991786, "longitude": -73.63831039274471 },
+        { "latitude": 45.457401696339495, "longitude": -73.6375754674492 },
+        { "latitude": 45.45760488792228, "longitude": -73.63730188212018 },
+        { "latitude": 45.45751834344874, "longitude": -73.63715167841016 },
+        { "latitude": 45.45804889661062, "longitude": -73.63652940589715 },
+        { "latitude": 45.45814672856342, "longitude": -73.63669570286183 },
+        { "latitude": 45.458372493960546, "longitude": -73.63645966846035 },
+        { "latitude": 45.45877886939742, "longitude": -73.63718922933766 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Concordia Stadium" }
+    },
+    {
+      "name": "Richard J. Renaud Science Complex",
+      "coordinates": [
+        { "latitude": 45.45832357820032, "longitude": -73.64141102643212 },
+        { "latitude": 45.457714009027676, "longitude": -73.64189382404145 },
+        { "latitude": 45.45761241352442, "longitude": -73.64186163753214 },
+        { "latitude": 45.45743932446764, "longitude": -73.64200111240577 },
+        { "latitude": 45.45698308174489, "longitude": -73.64082764592109 },
+        { "latitude": 45.45720132614708, "longitude": -73.64065598453818 },
+        { "latitude": 45.45752492836107, "longitude": -73.64146735279328 },
+        { "latitude": 45.458158957548974, "longitude": -73.64098723736318 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Richard J. Renaud Science Complex" }
+    },
+    {
+      "name": "Administration Building",
+      "coordinates": [
+        { "latitude": 45.45798587017782, "longitude": -73.64006992186201 },
+        { "latitude": 45.45791437740967, "longitude": -73.64012624825328 },
+        { "latitude": 45.45780337530055, "longitude": -73.63983388746053 },
+        { "latitude": 45.45789180072719, "longitude": -73.63976951444195 },
+        { "latitude": 45.45791814018919, "longitude": -73.63983656966964 },
+        { "latitude": 45.45802161652789, "longitude": -73.63975476229184 },
+        { "latitude": 45.45800092127535, "longitude": -73.63967295491406 },
+        { "latitude": 45.45806488839503, "longitude": -73.63963138067288 },
+        { "latitude": 45.45809122777617, "longitude": -73.63969709479603 },
+        { "latitude": 45.45820881414903, "longitude": -73.63961126410459 },
+        { "latitude": 45.45817494929879, "longitude": -73.63950934015848 },
+        { "latitude": 45.45825490794031, "longitude": -73.6394449671399 },
+        { "latitude": 45.458374375346345, "longitude": -73.63976280891917 },
+        { "latitude": 45.45829817962843, "longitude": -73.63982181751955 },
+        { "latitude": 45.45827654378852, "longitude": -73.63976549112827 },
+        { "latitude": 45.45796517491213, "longitude": -73.6400095721571 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Administration Building" }
+    },{
+      "name": "F.C. Smith Building",
+      "coordinates": [
+        { "latitude": 45.45875347104046, "longitude": -73.63957237208639 },
+        { "latitude": 45.45874030150613, "longitude": -73.63962601626855 },
+        { "latitude": 45.458663165600235, "longitude": -73.63968770707802 },
+        { "latitude": 45.458621775558505, "longitude": -73.63966624940517 },
+        { "latitude": 45.458580385486336, "longitude": -73.6395804187137 },
+        { "latitude": 45.45854840222796, "longitude": -73.63959651196836 },
+        { "latitude": 45.45849007976893, "longitude": -73.63944630825833 },
+        { "latitude": 45.45852018169079, "longitude": -73.63942485058546 },
+        { "latitude": 45.4584562150876, "longitude": -73.63924782478435 },
+        { "latitude": 45.4584411641116, "longitude": -73.63926123582989 },
+        { "latitude": 45.458414824893914, "longitude": -73.63919418060219 },
+        { "latitude": 45.458439282739306, "longitude": -73.63917808734755 },
+        { "latitude": 45.458412943520756, "longitude": -73.63910298549253 },
+        { "latitude": 45.458429875876966, "longitude": -73.63908689223787 },
+        { "latitude": 45.45842046901302, "longitude": -73.6390573879377 },
+        { "latitude": 45.4583941297857, "longitude": -73.63907079898323 },
+        { "latitude": 45.458384722915795, "longitude": -73.63904129468304 },
+        { "latitude": 45.458518300321145, "longitude": -73.63894205294605 },
+        { "latitude": 45.45852958853809, "longitude": -73.63896887503714 },
+        { "latitude": 45.45852206306038, "longitude": -73.63901447259198 },
+        { "latitude": 45.45853899538381, "longitude": -73.63900642596464 },
+        { "latitude": 45.45855969043888, "longitude": -73.6390600701468 },
+        { "latitude": 45.45860296189311, "longitude": -73.63906543456501 },
+        { "latitude": 45.45861801282594, "longitude": -73.63910030328341 },
+        { "latitude": 45.45861236872656, "longitude": -73.6391271253745 },
+        { "latitude": 45.45860296189311, "longitude": -73.63913785421093 },
+        { "latitude": 45.458672572423694, "longitude": -73.63932560884847 },
+        { "latitude": 45.45872901333137, "longitude": -73.6392987867574 },
+        { "latitude": 45.45874594559264, "longitude": -73.63934438431222 },
+        { "latitude": 45.45869514879355, "longitude": -73.63937925303063 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "F.C. Smith Building" }
+    },
+    {
+      "name": "Central Building",
+      "coordinates": [
+        { "latitude": 45.45851830031776, "longitude": -73.64068548886821 },
+        { "latitude": 45.458380960164014, "longitude": -73.64079814165075 },
+        { "latitude": 45.458079939478864, "longitude": -73.64001761880039 },
+        { "latitude": 45.45821916174548, "longitude": -73.63990764822695 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Central Building" }
+    },{
+      "name": "Loyola Jesuit Hall and Conference Centre",
+      "coordinates": [
+        { "latitude": 45.458416706266256, "longitude": -73.64085446803853 },
+        { "latitude": 45.45853711401402, "longitude": -73.64075790851065 },
+        { "latitude": 45.458587910955494, "longitude": -73.64088129012961 },
+        { "latitude": 45.458680097880546, "longitude": -73.64080350606548 },
+        { "latitude": 45.45880426778624, "longitude": -73.6411548754586 },
+        { "latitude": 45.45850513073109, "longitude": -73.64137481660545 },
+        { "latitude": 45.458484435656004, "longitude": -73.64131849021418 },
+        { "latitude": 45.45855874975436, "longitude": -73.64126886934555 },
+        { "latitude": 45.45851077484157, "longitude": -73.64113878220383 },
+        { "latitude": 45.45842893519026, "longitude": -73.64120315522241 },
+        { "latitude": 45.458381900854164, "longitude": -73.64107843249892 },
+        { "latitude": 45.45847314743046, "longitude": -73.64100735395755 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Loyola Jesuit Hall and Conference Centre" }
+    },
+    {
+      "name": "Psychology Building",
+      "coordinates": [
+        { "latitude": 45.45877745840601, "longitude": -73.6408511152623 },
+        { "latitude": 45.45864200024909, "longitude": -73.6404742649268 },
+        { "latitude": 45.45906060253183, "longitude": -73.64015776428454 },
+        { "latitude": 45.45922710190313, "longitude": -73.64057752996271 },
+        { "latitude": 45.4591462039659, "longitude": -73.64064190297375 },
+        { "latitude": 45.45912174642714, "longitude": -73.64059898763306 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Psychology Building" }
+    },
+    {
+      "name": "St. Ignatius of Loyola Church",
+      "coordinates": [
+        { "latitude": 45.45816413135461, "longitude": -73.64252079036207 },
+        { "latitude": 45.45794212790117, "longitude": -73.64200848848253 },
+        { "latitude": 45.457618528081426, "longitude": -73.64229280261463 },
+        { "latitude": 45.45762793507913, "longitude": -73.64235315231248 },
+        { "latitude": 45.45757055236869, "longitude": -73.64239874986197 },
+        { "latitude": 45.457639223474324, "longitude": -73.64257309343354 },
+        { "latitude": 45.45772953055444, "longitude": -73.6425100615269 },
+        { "latitude": 45.457748344511245, "longitude": -73.64258784558191 },
+        { "latitude": 45.45791108497575, "longitude": -73.64247921612576 },
+        { "latitude": 45.457983518446255, "longitude": -73.64266697075504 },
+        { "latitude": 45.45816601274064, "longitude": -73.64252481368821 },
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "St. Ignatius of Loyola Church" }
+    },
+    {
+      "name": "Jesuit Residence",
+      "coordinates": [
+        { "latitude": 45.458425642789216, "longitude": -73.64340457820938 },
+        { "latitude": 45.458350387828226, "longitude": -73.64314708616521 },
+        { "latitude": 45.45850089764977, "longitude": -73.64303443339588 },
+        { "latitude": 45.458596847451325, "longitude": -73.64329728985764 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Jesuit Residence" }
+    },
+    {
+      "name": "Applied Science Hub",
+      "coordinates": [
+        { "latitude": 45.45872666162977, "longitude": -73.64191058785205 },
+        { "latitude": 45.45844257514255, "longitude": -73.64211980013793 },
+        { "latitude": 45.45829582791963, "longitude": -73.64172015102771 },
+        { "latitude": 45.45857427104306, "longitude": -73.64150825653303 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Applied Science Hub" }
+    },
+    {
+      "name": "Hingston Hall, Wing HB",
+      "coordinates": [
+        { "latitude": 45.4595544551655, "longitude": -73.64198032524024 },
+        { "latitude": 45.45952811646791, "longitude": -73.64200178291058 },
+        { "latitude": 45.459503659094814, "longitude": -73.6419481387347 },
+        { "latitude": 45.459336219870956, "longitude": -73.6420822491744 },
+        { "latitude": 45.45936067731669, "longitude": -73.64214662218542 },
+        { "latitude": 45.459163136105694, "longitude": -73.64230219029545 },
+        { "latitude": 45.45913867857426, "longitude": -73.64224586391079 },
+        { "latitude": 45.45910105158214, "longitude": -73.64226732158113 },
+        { "latitude": 45.45896183149298, "longitude": -73.6419079056028 },
+        { "latitude": 45.45899757722434, "longitude": -73.64188376572366 },
+        { "latitude": 45.45896935691199, "longitude": -73.64181134608624 },
+        { "latitude": 45.45936820268246, "longitude": -73.6414975276574 },
+        { "latitude": 45.4595544551655, "longitude": -73.64197764303144 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Hingston Hall, Wing HB" }
+    },
+    {
+      "name": "Hingston Hall, Wing HC",
+      "coordinates": [
+        { "latitude": 45.45989121314983, "longitude": -73.64199910069622 },
+        { "latitude": 45.459791502896614, "longitude": -73.64207688475125 },
+        { "latitude": 45.45980279085867, "longitude": -73.64210907125675 },
+        { "latitude": 45.459629708525625, "longitude": -73.64224586390523 },
+        { "latitude": 45.45953940448878, "longitude": -73.6420178761578 },
+        { "latitude": 45.45971624975855, "longitude": -73.6418810835093 },
+        { "latitude": 45.45972753773568, "longitude": -73.64191058780604 },
+        { "latitude": 45.45983101075388, "longitude": -73.64183548595983 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Hingston Hall, Wing HC" }
+    },
+    {
+      "name": "Hingston Hall, Wing HA",
+      "coordinates": [
+        { "latitude": 45.459688029804376, "longitude": -73.64134732392708 },
+        { "latitude": 45.45943969361193, "longitude": -73.6415458073778 },
+        { "latitude": 45.45924967805933, "longitude": -73.64107105642135 },
+        { "latitude": 45.45950177776257, "longitude": -73.64086989076185 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Hingston Hall, Wing HA" }
+    },
+    {
+      "name": "Terrebonne Building",
+      "coordinates": [
+        { "latitude": 45.46008122654725, "longitude": -73.64089939504382 },
+        { "latitude": 45.459978694648704, "longitude": -73.64097449689005 },
+        { "latitude": 45.459939657225505, "longitude": -73.6408739140603 },
+        { "latitude": 45.459966936391574, "longitude": -73.64085178583774 },
+        { "latitude": 45.45993918689496, "longitude": -73.64077534288712 },
+        { "latitude": 45.46000362214579, "longitude": -73.64072706312885 },
+        { "latitude": 45.46003231227032, "longitude": -73.64080216497507 },
+        { "latitude": 45.46004171886531, "longitude": -73.64079613000528 }
+      ],
+      "fillColor": "#800000",
+      "strokeColor": "#000000",
+      "popUp": { "name": "Terrebonne Building" }
     }
+  
   ]
 };
 
