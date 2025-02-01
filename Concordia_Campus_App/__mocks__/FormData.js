ef93d7f087adc6cce43c1bbcfe5f58949739a327
jonathan.dannessa <jonathan.dannessa@ivans.com>
2025-02-01 17:08:36 -0500

    fix unit tests pipeline

---
+++ Concordia_Campus_App/__mocks__/FormData.js
@@ -0,0 +1,15 @@
+// __mocks__/FormData.js
+class FormData {
+    append() {}
+    get() {}
+    getAll() {}
+    has() {}
+    set() {}
+    delete() {}
+    keys() {}
+    values() {}
+    entries() {}
+    forEach() {}
+  }
+  
+  export default FormData;
