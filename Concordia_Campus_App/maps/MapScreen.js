6f86f7c40b4f0f3afec6045566cab95bee406edd
Joseph <51335923+Otterboi@users.noreply.github.com>
2025-02-08 16:37:03 -0500

    revert MapScreen.js

--- Concordia_Campus_App/maps/MapScreen.js
+++ Concordia_Campus_App/maps/MapScreen.js
@@ -1,10 +1,8 @@
-
-import { duration } from 'moment-timezone';
 import React, { useState, useRef, useEffect } from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import 'react-native-get-random-values';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
-import MapView, { Polygon, Marker, Polyline } from 'react-native-maps';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import MapView, { Polygon, Marker } from 'react-native-maps';
 import styles from './styles/mapScreenStyles'; 
 import buildingsData from './buildingCoordinates.js';
 import BuildingPopup from './BuildingPopup'; 
@@ -12,47 +10,34 @@ import { API_KEY } from '@env';
 import ShuttleBusMarker from './ShuttleBusMarker';
 import { getLocation } from './locationUtils';
 import MapDirections from './MapDirections';
-import MapViewDirections from 'react-native-maps-directions';
 
 const MapScreen = () => {
   const [campus, setCampus] = useState('SGW');
   const [zoomLevel, setZoomLevel] = useState(0.005); 
   const [selectedBuilding, setSelectedBuilding] = useState(null); 
-  const [showDirections, setShowDirections] = useState(false);
   const mapRef = useRef(null);
   const [selectedMarker, setSelectedMarker] = useState(null);
-  const [eta, setEta] = useState(null);
   const [shuttleStop, setShuttleStop] = useState(null);
-  const [distance, setDistance] = useState(null);
-  const [mode, setMode] = useState('DRIVING');
   const [toggleMapDirections, setToggleMapDirections] = useState(false);
   const [userLocation, setUserLocation] = useState(null);
 
+
   const campusLocations = {
     SGW: {
-      latitude: 45.49532997441208, // Concordia University SGW campus
+      latitude: 45.49532997441208,
       longitude: -73.57859533082366,
       title: 'SGW Campus',
       description: 'A well-known university located in Montreal, Canada',
     },
     Loyola: {
-      latitude: 45.458161998720556, // Concordia Loyola campus
+      latitude: 45.458161998720556,
       longitude: -73.63905090035233,
       title: 'Loyola Campus',
       description: 'Loyola Campus of Concordia University',
     },
   };
 
-  // Current campus location
   const location = campusLocations[campus];
-  const destinationLocation = campus === 'SGW' ? campusLocations.Loyola : campusLocations.SGW;
-  const directionsText = campus === 'SGW' ? '   Get directions to Loyola' : '   Get directions to SGW';
-
-
-  const handleDirections = (result) => {
-    setEta(result.duration);
-    setDistance(result.distance);
-  };
 
 
   useEffect(() => {
@@ -71,7 +56,7 @@ const MapScreen = () => {
         if(interval) clearInterval(interval);
       };
     }, [toggleMapDirections,shuttleStop]);
-  
+
 
   async function moveToLocation(latitude, longitude) {
     mapRef.current.animateToRegion(
@@ -88,24 +73,12 @@ const MapScreen = () => {
   const handleZoomIn = () => {
     // Zoom in by decreasing the delta more significantly
     setZoomLevel((prevZoom) => Math.max(prevZoom * 0.7, 0.0005)); // Zoom in more per click
-
   };
 
-  const handleCampusToggle = () => {
-    setShowDirections(false);
-    setEta(null);
-    setDistance(null);
-    setCampus(campus === 'SGW' ? 'Loyola' : 'SGW');
+  const handleZoomOut = () => {
+    // Zoom out by increasing the delta more significantly
+    setZoomLevel((prevZoom) => Math.min(prevZoom / 0.7, 0.05)); // Zoom out more per click
   };
-
-
-  useEffect(() => {
-    return () => {
-      setShowDirections(false);
-      setEta(null);
-      setDistance(null);
-    };
-  }, []);
 
 
   const handlePolygonPress = (building) => {
@@ -120,14 +93,12 @@ const MapScreen = () => {
     setSelectedBuilding(null); // Close the popup by clearing the selected building
   };
 
-
   return (
     <View style={styles.container}>
-      {/* SEARCH BAR */}
       <View style={styles.searchBarContainer}>
-        <TextInput
+        <GooglePlacesAutocomplete
+          fetchDetails={true}
           placeholder="Search Building or Class..."
-
           styles={{
             textInput: styles.searchBar, 
           }}
@@ -148,11 +119,10 @@ const MapScreen = () => {
         />
       </View>
 
-      {/* TOGGLE BUTTON FOR CAMPUSES */}
       <View style={styles.toggleButtonContainer}>
         <TouchableOpacity
           style={styles.toggleButton}
-          onPress={handleCampusToggle} // Switch between campus locations
+          onPress={() => setCampus(campus === 'SGW' ? 'Loyola' : 'SGW')}
         >
           <Text style={styles.toggleButtonText}>
             <Text style={campus === 'SGW' ? styles.highlightedText : styles.normalText}>SGW</Text>
@@ -162,32 +132,31 @@ const MapScreen = () => {
         </TouchableOpacity>
       </View>
 
-      {/* Map view */}
       <MapView
+        ref = {mapRef}
         style={styles.map}
         initialRegion={{
-          latitude: location.latitude, // Center map at Concordia University
-          longitude: location.longitude, // Center map at Concordia University
-          latitudeDelta: 0.0001, // Zoom level for a closer view
-          longitudeDelta: 0.0001, // Zoom level for a closer view
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: zoomLevel, // Use zoomLevel for zooming
+          longitudeDelta: zoomLevel,
         }}
         region={{
           latitude: location.latitude,
           longitude: location.longitude,
-          latitudeDelta: 0.0001,
-          longitudeDelta: 0.0001,
+          latitudeDelta: zoomLevel,
+          longitudeDelta: zoomLevel,
         }}
       >
-        
-              
+
         <ShuttleBusMarker setToggleMapDirections={setToggleMapDirections} setShuttleStop={setShuttleStop}/>
-          
+
         {toggleMapDirections && userLocation && shuttleStop && (
           <MapDirections 
             userLocation={userLocation} 
             destinationLocation={shuttleStop}/>
         )}
-         
+
         {selectedMarker && (
           <Marker
             coordinate={{
@@ -200,8 +169,8 @@ const MapScreen = () => {
               zIndex: 1000,
             }}
           />
-
         )}
+
         {buildingsData.buildings.map((building, index) => (
           <Polygon
             key={index}
@@ -213,94 +182,20 @@ const MapScreen = () => {
           />
         ))}
       </MapView>
-
-        <Marker coordinate={location} title={location.title} description={location.description} />
-        <Marker coordinate={destinationLocation} title={destinationLocation.title} description={destinationLocation.description} />
-        {/* Marker for Concordia University */}
-        <Marker
-          coordinate={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-          }}
-          title={location.title}
-          description={location.description}
-        />
-        {showDirections && (
-          <>
-            {/* For driving mode (always blue) */}
-            {mode === 'DRIVING' && (
-              <MapViewDirections
-                origin={location}
-                destination={destinationLocation}
-                apikey={API_KEY}
-                strokeWidth={5}
-                strokeColor="blue"  // Driving mode is always blue
-                mode={mode}
-                onReady={handleDirections}
-              />
-            )}
-            
-            {/* For walking mode (dashed blue line) */}
-            {mode === 'WALKING' && (
-            <MapViewDirections
-              origin={location}
-              destination={destinationLocation}
-              apikey={API_KEY}
-              strokeWidth={5}
-              strokeColor="blue"
-              mode={mode}
-              onReady={handleDirections}
-              lineDashPattern={[2, 10]}  // Small dots (short lines with large gaps)
-            />
-          )}
-            {/* Transit Mode */}
-            {mode === 'TRANSIT' && (
-              <MapViewDirections
-                origin={location}
-                destination={destinationLocation}
-                apikey={API_KEY}
-                strokeWidth={5}
-                strokeColor="green"  // Change color for transit mode
-                mode="TRANSIT"
-                onReady={handleDirections}
-              />
-            )}
-        </>
-      )};
-
-      <TouchableOpacity
-         style={styles.directionsButton}
-         onPress={() => setShowDirections(true)}
-      >
-      <View style={styles.directionsButton}>
-      <Image 
-      source={require('../assets/arrow.png')}  
-      style={styles.buttonImage} 
-    />
-    {/* Text for the directions button */}
-    <Text style={styles.directionsButtonText}>{directionsText}</Text>
-  </View>
-</TouchableOpacity>
-
-  <View style={styles.modeContainer}>
-    <TouchableOpacity onPress={() => setMode('DRIVING')} style={styles.modeButton}><Text style={styles.modeText}>Driving</Text></TouchableOpacity>
-    <TouchableOpacity onPress={() => setMode('WALKING')} style={styles.modeButton}><Text style={styles.modeText}>Walking</Text></TouchableOpacity>
-    <TouchableOpacity onPress={() => setMode('TRANSIT')} style={styles.modeButton}><Text style={styles.modeText}>Transit</Text></TouchableOpacity>
-  </View>
-
       {/* Render the BuildingPopup component with the close handler */}
       <BuildingPopup building={selectedBuilding} onClose={handleClosePopup} />
 
-
-      {eta !== null && distance !== null && (
-        <View style={[styles.routeInfoContainer, { flexDirection: 'row'}]}>
-          <Text style={styles.routeInfoText}>Distance: {Math.round(distance)} km</Text>
-          <Text style={styles.routeInfoText}>      ETA: {Math.round(eta)} mins</Text>
-        </View>
-      )}
+      {/* Zoom in/out buttons */}
+      <View style={styles.zoomButtonContainer}>
+        <TouchableOpacity style={styles.zoomButton} onPress={handleZoomIn}>
+          <Text style={styles.zoomButtonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.zoomButton} onPress={handleZoomOut}>
+          <Text style={styles.zoomButtonText}>−</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default MapScreen;
-
