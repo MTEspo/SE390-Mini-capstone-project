0859000a9d7fa3730bc389db6215832c9393ce6a
Joseph <51335923+Otterboi@users.noreply.github.com>
2025-02-25 17:50:01 -0500

    creating test files

---
+++ Concordia_Campus_App/__tests__/BottomSheet.test.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react-native";
+import BottomSheetComponent from "../shuttle_bus/bottom_sheet";
---
+++ Concordia_Campus_App/__tests__/ShuttleDirections.test.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react-native";
+import DirectionsDisplay from "../shuttle_bus/shuttle_directions";
