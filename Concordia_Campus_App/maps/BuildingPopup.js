ad5a9f214670698c50282dbe7fe646ef3d22ff69
Joseph <51335923+Otterboi@users.noreply.github.com>
2025-02-25 00:33:43 -0500

    merged template for indoor maps

--- Concordia_Campus_App/maps/BuildingPopup.js
+++ Concordia_Campus_App/maps/BuildingPopup.js
@@ -1,11 +1,19 @@
 // BuildingPopup.js
 
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
 import styles from './styles/popupStyles';  // Importing styles from the new file
 
 const BuildingPopup = ({ building, onClose }) => {
   if (!building) return null;
+
+  const buildingsWithButton = [
+    "John Molson School of Business",
+    "Henry F.Hall Building",
+    "Vanier Library Building",
+    "Central Building",
+    "Vanier Extension"
+  ];
 
   return (
     <View style={styles.popupContainer} testID="building-popup">
@@ -15,6 +23,14 @@ const BuildingPopup = ({ building, onClose }) => {
       <Text style={styles.popupTitle}>{building.popUp.name}</Text>
       <Text style={styles.popupText}>{building.popUp.address}</Text>
       <Text style={styles.popupText}>{building.popUp.codes}</Text>
+
+      {/* Conditional Button Rendering */}
+      {buildingsWithButton.includes(building.popUp.name) && (
+        <TouchableOpacity style={styles.bottomButton}>
+          <Button title="Access inside -->" onPress={() => { /* Define the button press action */ }} />
+        </TouchableOpacity>
+      )}
+
     </View>
   );
 };
