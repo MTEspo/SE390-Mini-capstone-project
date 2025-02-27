059b7bebbf0c026c8b47c91f859e697d256f49ae
jonathan.dannessa <jonathan.dannessa@ivans.com>
2025-02-26 20:10:13 -0500

    indoor floor config file data

---
+++ Concordia_Campus_App/maps/indoorFloorCoordinates.js
@@ -0,0 +1,36 @@
+const buildingsData = {
+"buildings": [
+    {
+        "name": "Hall Building",
+        "floor-2":{
+            "classCoordinates": {
+                "831": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10 },
+                "820": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10},
+            },
+            "node_1":{ "latitude": 45.495359016476655, "longitude": -73.57936484536624, "connected_nodes":[]},
+            "node_2":{ "latitude": 45.495359016476655, "longitude": -73.57936484536624, "connected_nodes":[]},
+            "path_to_escalator_up":[
+                { "latitude": 45.495359016476655, "longitude": -73.57936484536624},
+                { "latitude": 45.495359016476655, "longitude": -73.57936484536624 },
+                { "latitude": 45.495359016476655, "longitude": -73.57936484536624 }
+            ],
+            "esclator_up": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10 },
+            "esclator_down":{ "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10 },
+            "elevator": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10 },
+            "floorImage": "",
+        },
+        "floor-8":{
+            "classCoordinates": {
+                "831": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10 },
+                "820": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node":10},
+            },
+            "node_1":{ "latitude": 45.495359016476655, "longitude": -73.57936484536624, "connected_nodes":[]},
+            "node_2":{ "latitude": 45.495359016476655, "longitude": -73.57936484536624, "connected_nodes":[]},
+            "esclator_up": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node": 10 },
+            "esclator_down":{ "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node": 10 },
+            "elevator": { "latitude": 45.495359016476655, "longitude": -73.57936484536624, "last_connecting_node": 10 },
+            "floorImage": "",
+        },
+    },
+]
+}
